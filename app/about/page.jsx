a8f0641350ba2032d38951c5461cb68ae8e46daf
hannaofficial <hanna.iithyderabad@gmail.com>
2024-10-03 03:00:57 +0530

    about change

--- app/about/page.jsx
+++ app/about/page.jsx
@@ -1,9 +1,13 @@
+import Image from 'next/image'
+import Link from 'next/link'
 import React from 'react'
+import Logo from '@/public/assets/ownerPic.jpeg'
 
 const AboutPage = () => {
   return (
-    <div className='min-h-screen flex justify-center items-center'>
-      <p className='text-xl sm:text-4xl font-mono font-medium text-neutral-400 mx-4 text-center'>Hi I am Hanna. This website is in developing phase.<br/> my github: @hannaofficial</p>
+    <div className='min-h-screen flex flex-col gap-6 justify-center items-center'>
+      <Image src={Logo} alt='profile-pic' height={200} width={200} className='rounded-full w-24 h-24 border hover:border-green-600 hover:-translate-y-10 transition-all duration-500 ease-linear shadow-lg' priority/>
+      <p className='text-xl sm:text-4xl font-mono font-medium text-neutral-400 mx-4 text-center'>Hi I am Hanna. This website is in developing phase.<br/> my github: <Link href='https://github.com/hannaofficial' target='_blank' className='text-[#2478c1] underline hover:text-green-800'>@hannaofficial</Link> </p>
       
     </div>
   )
